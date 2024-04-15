52e59e4334ce58fb552e4692d1dc4af9140f5121
parvathy003 <167087751+parvathy003@users.noreply.github.com>
2024-04-15 22:50:30 +1000

    Finished Part 1

--- A1.py
+++ A1.py
@@ -1,10 +1,21 @@
 import sys
 
-reward_points = 0
+customers = {
+    "Kate": 20,
+    "Tom": 32
+}
+
+product_info = {
+    "vitaminC": 12.0,   
+    "vitaminE": 14.5,   
+    "coldTablet": 6.4,  
+    "vaccine": 32.6,    
+    "fragrance": 25.0   
+}
 def get_user_input(prompt): # Function to get user input based on prompt sent to this function
-    sys.stdout.write(prompt + ": ")  # Display prompt
-    sys.stdout.flush()  # Ensure prompt is displayed immediately
-    return sys.stdin.readline().strip()  # Read user input and strip newline character
+    sys.stdout.write(prompt + ": ") 
+    sys.stdout.flush() 
+    return sys.stdin.readline().strip()  
 
 # Example usage:
 
@@ -27,19 +38,22 @@ def print_receipt(name,product,price,qty,rewardPoints):
 
 
 name = get_user_input("What's your name: ")
-print("Hello,", name)
-
 
 product = get_user_input("What product do you want? ")
-print("Product:" ,product)
 
 quantity = get_user_input("what quantity do you need? ")
-print("quantity:" , quantity)
 
 price = calc_total_cost(12,int(quantity))
 print("Total Cost price:" , price)
 
-reward_points+= round(price)
+reward_points = round(price)
 print("Reward points: ", reward_points)
 
 print_receipt(name,product,price,quantity,reward_points)
+
+# Checks if new user or already existing user
+if( name in customers):
+    customers[name] += reward_points
+else:
+    customers[name] = reward_points
+print(customers)
