287ad6b30151541f862e443062948dcd04422269
parvathy003 <167087751+parvathy003@users.noreply.github.com>
2024-04-16 21:28:33 +1000

    Solved correction Q9-10

--- A1.py
+++ A1.py
@@ -6,7 +6,7 @@ customers = {
 }
 
 product_info = {
-    "vitaminC": 12.0,   
+    "vitaminC": (12.0,"n"),   
     "vitaminE": 14.5,   
     "coldTablet": 6.4,  
     "vaccine": 32.6,    
@@ -17,12 +17,19 @@ def get_user_input(prompt): # Function to get user input based on prompt sent to
     sys.stdout.flush() 
     return sys.stdin.readline().strip()  
 
-# Example usage:
 
 def calc_total_cost(price, qty):
     print(price, qty ,price*qty)
     return price*qty
 
+def round_reward_points(rewardPoints):
+    roundVal = rewardPoints- int(rewardPoints)
+    print(roundVal)
+    if (roundVal < 0.5):
+        return rewardPoints
+    else: 
+        return int(rewardPoints)+1
+    
 def print_receipt(name,product,price,qty,rewardPoints):
     print("---------------------------------------------------------")
     print("                     Receipt                             ")
@@ -37,16 +44,16 @@ def print_receipt(name,product,price,qty,rewardPoints):
 
 
 
-name = get_user_input("What's your name: ")
+name = get_user_input("What's your name: ") # Customer name to be entered only in alphabets
 
-product = get_user_input("What product do you want? ")
+product = get_user_input("What product do you want? ") # Product entered should always be vaild
 
-quantity = get_user_input("what quantity do you need? ")
+quantity = get_user_input("what quantity do you need? ") #
 
-price = calc_total_cost(12,int(quantity))
+price = calc_total_cost(product_info[product],int(quantity))
 print("Total Cost price:" , price)
 
-reward_points = round(price)
+reward_points = round_reward_points(price)
 print("Reward points: ", reward_points)
 
 print_receipt(name,product,price,quantity,reward_points)
