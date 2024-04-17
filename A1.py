4b213eae5865d7fa83272f79ec74cded69153fa9
parvathy003 <167087751+parvathy003@users.noreply.github.com>
2024-04-17 11:28:35 +1000

    fixed part 1

--- A1.py
+++ A1.py
@@ -7,16 +7,23 @@ customers = {
 
 product_info = {
     "vitaminC": (12.0,"n"),   
-    "vitaminE": 14.5,   
-    "coldTablet": 6.4,  
-    "vaccine": 32.6,    
-    "fragrance": 25.0   
+    "vitaminE": (14.5,"n"),  
+    "coldTablet":(6.4,"n"), 
+    "vaccine": (32.6,"y"),   
+    "fragrance": (25.0 ,"n") 
 }
-def get_user_input(prompt): # Function to get user input based on prompt sent to this function
+
+# Function to get user input based on prompt sent to this function
+def get_user_input(prompt):
     sys.stdout.write(prompt + ": ") 
     sys.stdout.flush() 
     return sys.stdin.readline().strip()  
 
+####################################DOUBT################################
+
+def prescription_required(product, product_info):
+    product_list = product_info.get(product,(0, "n"))
+    return product_list[1] == "y"
 
 def calc_total_cost(price, qty):
     print(price, qty ,price*qty)
@@ -43,24 +50,63 @@ def print_receipt(name,product,price,qty,rewardPoints):
     print(" Earned reward: "+ str(rewardPoints))
 
 
+# Customer name to be entered only in alphabets
+while True:
+    name = get_user_input("What's your name: ") 
+    if name.isalpha():
+        break
+    else:
+        print("Sorry,please enter a valid name.The entered name is invalid")
 
-name = get_user_input("What's your name: ") # Customer name to be entered only in alphabets
+        
+# Product entered should always be vaild
+while True:
+    product = get_user_input("What product do you want? ")
+    if product in product_info:
+        break
+    else:
+        print("You have entered an invaild product name. Enter a valid product name") 
 
-product = get_user_input("What product do you want? ") # Product entered should always be vaild
+# Check if the product requires a prescription and handle accordingly
+if product_info[product][1] == "y":
+        prescription_answer=""
+quantity=0
+while True:
+    prescription_answer = get_user_input(f"Product '{product}' requires a doctor's prescription. Do you have a prescription? (y/n)")
+    if prescription_answer in ["y","n"]:
+        if prescription_answer.lower() != 'y':
+            print("The product requires a prescription by doctor.Do provide a prescription for purchasing it.")
+            sys.exit()
+        
+        while True:
+            quantity = get_user_input("what quantity do you need? ")
+            if quantity.isdigit() and int(quantity) > 0:
+                break
+            else:
+                print("Entered invalid quantity.Enter a positive integer.")
+                
+        break
+    else:
+        print("Please enter a valid choice. Do you have a prescription (y/n)?")
 
-quantity = get_user_input("what quantity do you need? ") #
 
-price = calc_total_cost(product_info[product],int(quantity))
+price = calc_total_cost(product_info[product][0],int(quantity)) 
 print("Total Cost price:" , price)
 
-reward_points = round_reward_points(price)
+# Calculating the rewrd points earned from purchase
+
+reward_points = round_reward_points(price) 
 print("Reward points: ", reward_points)
 
 print_receipt(name,product,price,quantity,reward_points)
 
 # Checks if new user or already existing user
 if( name in customers):
-    customers[name] += reward_points
+     customers[name] += reward_points
 else:
-    customers[name] = reward_points
-print(customers)
+     customers[name] = reward_points
+     print(customers)
+
+
+
+    
